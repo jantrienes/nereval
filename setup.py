7817f883a646c6de391e517ba339fb8dd77e6336
Jan Trienes <jan.trienes@googlemail.com>
2017-12-11 21:36:13 +0100

    bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='muceval',
-    version='0.2',
+    version='0.2.1',
     description='MUC-like evaluation script for named entity recognition systems.',
     license='MIT',
     py_modules=['muceval'],
