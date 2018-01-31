660caa42d853ba995521e5d65382e98b49b00343
Jan Trienes <jan.trienes@googlemail.com>
2018-01-31 16:37:04 +0100

    automate build for pypi and pypitest

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from distutils.core import setup
+from setuptools import setup
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -9,9 +9,10 @@ setup(
     version='0.2.3',
     author='Jan Trienes',
     author_email='jan.trienes@googlemail.com',
-    keywords=['ner', 'nlp', 'evaluation', 'f1_score', 'f1', 'linguistics', 'machine_learning'],
+    url='https://github.com/jantrienes/nereval',
     description='Evaluation script for named entity recognition systems based on F1 score.',
     long_description=read('README.rst'),
+    keywords=['ner', 'nlp', 'evaluation', 'f1_score', 'f1', 'linguistics', 'machine_learning'],
     license='MIT',
     py_modules=['nereval'],
     tests_require=[
